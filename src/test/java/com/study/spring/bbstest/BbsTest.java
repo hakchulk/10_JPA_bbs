7b5ed1d5c6b4e10245adf274699f799679f219bf
hakchulk <hakchulk@gmail.com>
2026-03-17 16:57:25 +0900

    260317

--- src/test/java/com/study/spring/bbstest/BbsTest.java
+++ src/test/java/com/study/spring/bbstest/BbsTest.java
@@ -15,14 +15,14 @@ public class BbsTest {
 
 	@Test
 	public void bbsInsert() {
-//		for (int i = 0; i < 3; i++) {
-//			Bbs bbs = new Bbs();
-//			bbs.setName("홍길동" + i);
-//			bbs.setTitle("title");
-//			bbs.setContent("content 안녕");
-//
-//			bbsRepo.save(bbs);
-//		}
+		for (int i = 0; i < 3; i++) {
+			Bbs bbs = new Bbs();
+			bbs.setName("홍길동" + i);
+			bbs.setTitle("title");
+			bbs.setContent("content 안녕");
+
+			bbsRepo.save(bbs);
+		}
 		Bbs bbs = Bbs.builder().name("나나3").title("나나생일").content("생일추카").build();
 		bbsRepo.save(bbs);
 	}
